599af1500078e583f3a3dcc7f068900bef228a3a
sidharth00007 <championsid07@gmail.com>
2022-11-19 19:57:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Clients from "./components/Clients";
-import Footer from "./components/Footer";
+import React, {  useEffect } from "react";
 // import Free from "./components/Free";
 import HomePage from "./HomePage";
-import Like from "./components/Like";
-import Navbar from "./components/Navbar";
-import Release from "./components/Release";
-import ScrollToTop from "./components/ScrollToTop";
-import Signup from "./components/Signup";
-import SuperRare from "./components/SuperRare";
+
 import scrollreveal from "scrollreveal";
 import "./sass/index.scss";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Buyer from "./components/Buyer";
 
 
 function App() {
-  const [theme, setTheme] = useState("dark");
-  const changeTheme = () => {
-    theme === "dark" ? setTheme("light") : setTheme("dark");
-  };
+  // const [theme, setTheme] = useState("dark");
+  // const changeTheme = () => {
+  //   theme === "dark" ? setTheme("light") : setTheme("dark");
+  // };
   useEffect(() => {
     const registerAnimations = () => {
       const sr = scrollreveal({
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 import { ImSun } from "react-icons/im";
 import { BsFillMoonFill } from "react-icons/bs";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo1.png";
 export default function Navbar({ changeTheme, currentTheme }) {
   const [navState, setNavState] = useState(false);
   return (
     <nav>
       <div className="brand-container">
         <div className="brand">
-          <img src={logo} alt="logo" />
+          <img src={logo} height={100} width={100} alt="logo" />
         </div>
         <div className="toggle-container">
           <div className="toggle">
